6b0ce50bebcfd073c175da43466a70445f961d14
nicolas diaz almassio <nicolas.diaz.almassio@gmail.com>
2021-02-28 14:17:57 -0300

    added resctrictions to actions and views, added many to many association

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
     before_action :set_student, only: [:show, :edit, :update]
+    skip_before_action :require_user, only: [:new, :create]
+    before_action :require_same_student, only: [:edit, :update]
     def index
         @stundets = Student.all
     end
@@ -41,6 +43,13 @@ class StudentsController < ApplicationController
     end
     def student_params
         params.require(:student).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def require_same_student
+        if current_user != @student
+            flash[:notice] = "You can only edit your own profile"
+            redirect_to student_path(current_user)
+        end
     end
 
     
